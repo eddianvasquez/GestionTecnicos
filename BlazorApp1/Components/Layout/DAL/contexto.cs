edb35ee923bc6c680cceda89428f0ae32ba6d6f4
eddianvasquez <eddian_vasquez@ucne.edu.do>
2025-05-19 16:09:46 -0400

    agregando componentes necesarios

---
+++ BlazorApp1/Components/Layout/DAL/contexto.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using BlazorApp1.Components.Layout.models;
+
+
+namespace DAL
+{
+    public class Contexto : DbContext
+    {
+        public DbSet<Tecnico> Tecnicos { get; set; }
+
+        public Contexto(DbContextOptions<Contexto> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Tecnico>().HasIndex(t => t.Nombres).IsUnique();
+        }
+    }
+}
