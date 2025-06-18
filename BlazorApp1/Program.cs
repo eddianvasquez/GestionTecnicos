f585e08471464f1a13d599e659b21979d9c338f4
Ramon Gonzalez <ramon_gonzalez2@ucne.edu.do>
2025-06-18 13:30:21 -0400

    Agregando sistemas a program

--- BlazorApp1/Program.cs
+++ BlazorApp1/Program.cs
@@ -1,33 +1,41 @@
-using BlazorApp1.Components.models;
-using BlazorApp1.Components.services;
-using BlazorApp1.Components.Layout.DAL;
-using Microsoft.EntityFrameworkCore;
 using BlazorApp1.Components;
+using BlazorApp1.DAL;
+using BlazorApp1.Services;
+using Blazored.Toast;
+using Microsoft.EntityFrameworkCore;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Cadena de conexión
-var connectionString = builder.Configuration.GetConnectionString("SqlConStr");
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
 
-// Servicios
-builder.Services.AddRazorComponents().AddInteractiveServerComponents();
-builder.Services.AddScoped<TecnicosService>();
-builder.Services.AddDbContext<ContextoPrueba>(options =>
-    options.UseSqlServer(connectionString));
+var ConStr = builder.Configuration.GetConnectionString("SqlConStr");
+
+builder.Services.AddDbContextFactory<Contexto>(o => o.UseSqlServer(ConStr));
+
+builder.Services.AddScoped<SistemasService>();
+
+builder.Services.AddBlazoredToast();
 
 var app = builder.Build();
 
-// Middleware
+// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
+
+
 app.UseAntiforgery();
 
 app.MapStaticAssets();
-app.MapRazorComponents<App>().AddInteractiveServerRenderMode();
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
 
 app.Run();
