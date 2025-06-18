d87bc72ed21510fe6e237f9d4a8d50d7a95f7b75
Ramon Gonzalez <ramon_gonzalez2@ucne.edu.do>
2025-06-18 13:28:09 -0400

    Agregando componentes necesarios

---
+++ BlazorApp1/models/Sistema.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorApp1.models
+{
+    public class Sistemas
+    {
+        [Key]
+        public int SistemaId { get; set; }
+        [Required(ErrorMessage = "Este campo es requerido")]
+        public string Descripcion { get; set; }
+        [Range(1, 100, ErrorMessage = "El nivel de complejidad debe estar entre 1 y 100.")]
+        public double Complejidad { get; set; }
+    }
+}
